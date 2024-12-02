ccd56ac2dbc6c24f97f26bf68a9ddc194c89c429
soheila gholami <s.gholami1995@gmail.com>
2024-12-02 18:32:04 +0330

    hadoo changed app A

--- AppA.jsx
+++ AppA.jsx
@@ -6,6 +6,11 @@ const AppA = () => {
       <div>sohi changed app1 second div</div>
     </>
   );
+  return <>
+  <div>hadoo changed app1 first div</div>
+  <hr/>
+  <div>hadoo changed app1 second div</div>
+  </>;
 };
 
 export default AppA;
